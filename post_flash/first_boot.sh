049318c0565cfde585bef1ad875e893096da8ce6
Nataraj Basappa <nataraj.basappa@yahoo.com>
2020-05-10 16:33:52 +0100

    Changed to work with 20.04 LTS and made the script bit more resilient

--- post_flash/first_boot.sh
+++ post_flash/first_boot.sh
@@ -9,13 +9,17 @@ netplan generate
 echo "First boot - updating hostname" 
 echo "YOUR_HOSTNAME" > '/etc/hostname'
 
-# enable cgroup memory
-echo "First boot - enable cgroup memory"
-sed -i '$ s/$/ cgroup_enable=memory cgroup_memory=1/' /boot/firmware/nobtcmd.txt
+# letting iptable see bridge traffic and allowing ip forwarding
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.ipv4.tcp_mtu_probing = 1
+EOF
 
-# enable ip forwarding
-echo "First boot - enable ip forwarding"
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf 
+# enable cgroup memory
+echo "First boot - enable cgroup memory & cpu limit"
+sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1/' /boot/firmware/cmdline.txt
 
 # disable service by deleting self
 rm /wireless.yaml
--- post_flash/post_first_boot.sh
+++ post_flash/post_first_boot.sh
@@ -21,8 +21,27 @@ cat /id_rsa.pub >> /home/YOUR_USER_NAME/.ssh/authorized_keys
 
 # install k8s packages
 echo "Post first boot - installing k8s and dependent packages"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+curl -k https://packages.cloud.google.com/apt/doc/apt-key.gpg -o /tmp/k8s-key
+sleep 5 # giving time to write output key before reading
+apt-key add /tmp/k8s-key
+
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+# let docker use systemd for its driver management
+[[ -d /etc/docker/ ]] || mkdir -p /etc/docker/
+cat <<EOF | tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
 apt-get update -q && apt-get install -y apt-transport-https docker.io kubeadm kubelet kubectl
 apt-mark hold kubelet kubeadm kubectl
 systemctl enable docker
